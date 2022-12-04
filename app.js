acddfac3c8769ad2031a176b26d6d4ea5de03808
Evgeniy Yurchenkov <gear1995V@gmail.com>
2022-12-04 15:14:39 +0500

    fix/mongoose connection

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const { PORT = 3000 } = process.env;
 const app = express();
 const routes = require("./routes/users.js");
 const bodyParser = require("body-parser");
 
-mongoose.connect("mongodb://localhost:27017/mestodb");
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 
