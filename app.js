5b499298fa190057a8da71a7a2898c4589beb49d
Evgeniy Yurchenkov <gear1995V@gmail.com>
2022-12-04 14:44:06 +0500

    fix/routes

--- app.js
+++ app.js
@@ -4,19 +4,15 @@ const path = require("path");
 const { PORT = 3000 } = process.env;
 const app = express();
 const routes = require("./routes/users.js");
-const userRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
 const bodyParser = require("body-parser");
 
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-//app.use("/", routes);
-app.use("/users", userRouter);
-app.use("/cards", cardRouter);
-app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use("/", routes);
 
 app.use((req, res, next) => {
   req.user = {
