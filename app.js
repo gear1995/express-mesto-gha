4f3b0ed442ff266b5b4cc005a393ae22d731727d
Evgeniy Yurchenkov <gear1995V@gmail.com>
2022-12-05 20:14:29 +0500

    fix/eslint

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const userRouter = require('./routes/users');
-const cardRouter = require('./routes/cards');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/users");
+const cardRouter = require("./routes/cards");
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 });
 const { PORT = 3000 } = process.env;
@@ -13,21 +13,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
+app.use("/users", userRouter);
+app.use("/cards", cardRouter);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '637e319e55ee246cfc0c827c',
+    _id: "637e319e55ee246cfc0c827c",
   };
 
   next();
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+  res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
 });
 
 app.listen(PORT, () => {
- // console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
