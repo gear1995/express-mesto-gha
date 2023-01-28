83e3a4a4f393d541d8bbae070dac85ef71f4e127
Evgeniy Yurchenkov <gear1995V@gmail.com>
2023-01-28 23:47:55 +0500

    feat/routes currentUser

--- routes/users.js
+++ routes/users.js
@@ -11,14 +11,14 @@ const {
 
 const { validateUpdateAvatar, validateUpdateProfile, validateGetUsersById } = require('../middlewares/validator');
 
-router.get('/:userId', auth, validateGetUsersById, getUsersById);
+router.get('/me', getCurrentUser);
 
 router.get('/', auth, getUsers);
+
+router.get('/:userId', auth, validateGetUsersById, getUsersById);
 
 router.patch('/me', auth, validateUpdateProfile, updateProfile);
 
 router.patch('/me/avatar', auth, validateUpdateAvatar, updateAvatar);
-
-router.get('/me', getCurrentUser);
 
 module.exports = router;
