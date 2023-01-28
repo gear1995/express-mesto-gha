2eb2068cf5bc10c5d81d5503be49a963530403a2
Evgeniy Yurchenkov <gear1995V@gmail.com>
2023-01-28 22:58:57 +0500

    fix/regEx

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 function validateUrl(url) {
-  const regEx = /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/gm;
+  const regEx = /https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i;
 
   if (regEx.test(url)) {
     return url;
